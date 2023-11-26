dbda61482225937b9c07947ece86c326a84fa9b8
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-26 13:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ window.onload = function() {
+      // Retrieve browser information
+      var browserName = navigator.appName;
+      var browserVersion = navigator.appVersion;
+
+      // Display information in the browser-info div
+      var browserInfoDiv = document.getElementById('browser-info');
+      browserInfoDiv.textContent = "You are using " + browserName + " version " + browserVersion;
+    }
